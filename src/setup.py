121a1f13d5525329e057db0b99b838cbf7db5a8b
Sukil Etxenike <sukiletxe@yahoo.es>
2019-02-16 22:29:47 +0100

    Removed visual C++ redistributables

--- src/setup.py
+++ src/setup.py
@@ -3,22 +3,13 @@ import py2exe
 import platform
 from glob import glob
 import accessible_output2
-def get_data():
-  if platform.architecture()[0][:2] == "32":
-   return [
-   ("Microsoft.VC90.CRT", glob("windows_vc++/msvc32/Microsoft.VC90.CRT/*")),
-   ("Microsoft.VC90.MFC", glob("windows_vc++/msvc32/Microsoft.VC90.MFC/*")),]
-  elif platform.architecture()[0][:2] == "64":
-   return [
-   ("Microsoft.VC90.CRT", glob("windows_vc++/msvc64/Microsoft.VC90.CRT/*")),
-   ("Microsoft.VC90.MFC", glob("windows_vc++/msvc64/Microsoft.VC90.MFC/*")),]
 
 setup(
     name = "Virtual Braille n'Speak emulator",
     author = "Tyler Spivey, Sukil Etxenike",
     author_email = "sukiletxe@yahoo.es",
     version = "1.0",
-    data_files= get_data()+[("", ['../readme.html'])] + accessible_output2.find_datafiles(),
+    data_files= [("", ['../readme.html'])] + accessible_output2.find_datafiles(),
     packages = find_packages(),
     options = {
         "py2exe":{
